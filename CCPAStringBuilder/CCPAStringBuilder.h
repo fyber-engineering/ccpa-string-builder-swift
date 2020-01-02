12ba0d0759b5c61b270723ddc5f23a9f54cdce04
Nikita Fedorenko <nikita.fedorenko@fyber.com>
2020-01-02 10:47:36 +0200

    added initial project and workspace

---
+++ CCPAStringBuilder/CCPAStringBuilder.h
@@ -0,0 +1,19 @@
+//
+//  CCPAStringBuilder.h
+//  CCPAStringBuilder
+//
+//  Created by Nikita on 02/01/2020.
+//  Copyright © 2020 Fyber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CCPAStringBuilder.
+FOUNDATION_EXPORT double CCPAStringBuilderVersionNumber;
+
+//! Project version string for CCPAStringBuilder.
+FOUNDATION_EXPORT const unsigned char CCPAStringBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCPAStringBuilder/PublicHeader.h>
+
+
