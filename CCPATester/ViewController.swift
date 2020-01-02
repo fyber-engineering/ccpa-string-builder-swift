d1ca2c5b5c20c4b179e430b485ad1239fea672db
Nikita Fedorenko <nikita.fedorenko@fyber.com>
2020-01-02 12:26:18 +0200

    more workspace configuration

---
+++ CCPATester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CCPATester
+//
+//  Created by Nikita on 02/01/2020.
+//  Copyright © 2020 Fyber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
